7c47fac49c6cbe65ead18931cee7d8f552eebaf2
cjsellwood <c.sellwood22@gmail.com>
2021-01-10 15:01:36 +1100

    Added element images

--- src/components/Store.js
+++ src/components/Store.js
@@ -4,22 +4,28 @@ import "./Store.css";
 
 const Store = (props) => {
   const itemsArray = props.data.map((item) => (
-    <Link to={{
-      pathname: `/store/${item.name}`
-    }} className="store-item" key={item.number}>
-      <img className="product-image" src={item.src} alt={item.name} />
+    <Link
+      to={{
+        pathname: `/store/${item.name}`,
+      }}
+      className="store-item"
+      key={item.number}
+    >
+      <div>
+        <img className="product-image" src={item.src} alt={item.name} />
+      </div>
       <div className="product-details">
         <h2>{item.name}</h2>
-        <h2>
-          $
-          {props.formatPrice(item.price)}
-        </h2>
+        <h2>${props.formatPrice(item.price)}</h2>
       </div>
     </Link>
   ));
-  return <div className="store-container">
-    <div>
-      </div>{itemsArray}</div>;
+  return (
+    <div className="store-container">
+      <div></div>
+      {itemsArray}
+    </div>
+  );
 };
 
 export default Store;
--- src/inventory.js
+++ src/inventory.js
@@ -1,6 +1,21 @@
 // Store data
 //https://en.wikipedia.org/wiki/Prices_of_chemical_elements
+import Lithium from "./images/Lithium.jpg";
+import Magnesium from "./images/Magnesium.jpg";
+import Aluminium from "./images/Aluminium.jpg";
+import Titanium from "./images/Titanium.jpg";
+import Iron from "./images/Iron.jpg";
+import Cobalt from "./images/Cobalt.jpg";
+import Nickel from "./images/Nickel.jpg";
+import Copper from "./images/Copper.jpg";
+import Zinc from "./images/Zinc.jpg";
+import Silver from "./images/Silver.jpg";
+import Tin from "./images/Tin.jpg";
+import Tungsten from "./images/Tungsten.jpg";
+import Platinum from "./images/Platinum.jpg";
 import Gold from "./images/Gold.jpg";
+import Lead from "./images/Lead.jpg";
+import Uranium from "./images/Uranium.jpg";
 
 const inventory = [
   {
@@ -8,56 +23,56 @@ const inventory = [
     number: 3,
     price: 81.4,
     quantity: 0,
-    src: Gold,
+    src: Lithium,
   },
   {
     name: "Magnesium",
     number: 12,
     price: 2.32,
     quantity: 0,
-    src: Gold,
+    src: Magnesium,
   },
   {
     name: "Aluminium",
     number: 13,
     price: 1.79,
     quantity: 0,
-    src: Gold,
+    src: Aluminium,
   },
   {
     name: "Titanium",
     number: 22,
     price: 11.1,
     quantity: 0,
-    src: Gold,
+    src: Titanium,
   },
   {
     name: "Iron",
     number: 26,
     price: 0.424,
     quantity: 0,
-    src: Gold,
+    src: Iron,
   },
   {
     name: "Cobalt",
     number: 27,
     price: 32.8,
     quantity: 0,
-    src: Gold,
+    src: Cobalt,
   },
   {
     name: "Nickel",
     number: 28,
     price: 13.9,
     quantity: 0,
-    src: Gold,
+    src: Nickel,
   },
   {
     name: "Copper",
     number: 29,
     price: 6.0,
     quantity: 0,
-    src: Gold,
+    src: Copper,
   },
   {
     name: "Zinc",
@@ -71,28 +86,28 @@ const inventory = [
     number: 47,
     price: 521,
     quantity: 0,
-    src: Gold,
+    src: Silver,
   },
   {
     name: "Tin",
     number: 50,
     price: 18.7,
     quantity: 0,
-    src: Gold,
+    src: Tin,
   },
   {
     name: "Tungsten",
     number: 74,
     price: 35.3,
     quantity: 0,
-    src: Gold,
+    src: Tungsten,
   },
   {
     name: "Platinum",
     number: 78,
     price: 27800,
     quantity: 0,
-    src: Gold,
+    src: Platinum,
   },
   {
     name: "Gold",
@@ -106,14 +121,14 @@ const inventory = [
     number: 82,
     price: 2.0,
     quantity: 0,
-    src: Gold,
+    src: Lead,
   },
   {
     name: "Uranium",
     number: 92,
     price: 101,
     quantity: 0,
-    src: Gold,
+    src: Uranium,
   },
 ];
 
