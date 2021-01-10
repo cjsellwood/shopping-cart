4f6aa579474d150700f471eb92715a14409abdd1
cjsellwood <c.sellwood22@gmail.com>
2021-01-10 13:54:08 +1100

    Homepage complete

--- src/components/MetalMart.js
+++ src/components/MetalMart.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./MetalMart.css";
+import Logo from "../images/logo.png";
 
 const MetalMart = () => {
   return (
-    <div>
-      MetalMart Homepage
+    <div className="home-container">
+      <div>
+        <img className="home-image" src={Logo} alt="MetalMart" />
+      </div>
+      <h1>The destination for all your elemental metal needs</h1>
     </div>
-  )
-}
+  );
+};
 
-export default MetalMart
+export default MetalMart;
